0bbb428df7f64b756b71c0d9dc6aba6a359815d3
ShakDesign <shakirajensen@hotmail.com>
2017-05-29 09:52:27 +1200

    added script and css files

---
+++ js/tutorial.js
@@ -0,0 +1,81 @@
+var tour;
+
+    tour = new Shepherd.Tour({
+        defaults: {
+            classes: 'shepherd-theme-dark',
+            scrollTo: true
+        }
+    });
+
+    tour.addStep('Language', { 
+        text: 'Choose language',
+        attachTo: '#languageButton left',
+        buttons: [
+            {
+                text: 'Next',
+                action: tour.next,
+
+                
+            }, {
+                text: 'Skip',
+                action: function() {
+                return tour.hide();
+              }
+            }
+        ]
+    });
+
+    tour.addStep('Camera', { 
+        text: 'Tap the camera button to capture the object',
+        attachTo: '#cameraButton top',
+        buttons: [
+            {
+                text: 'Next',
+                action: tour.next,
+
+                
+            }, {
+                text: 'Skip',
+                action: function() {
+                return tour.hide();
+              }
+            }
+        ]
+    });
+
+    tour.addStep('Info', { 
+        text: 'Drag up to view the details',
+        attachTo: '#title top',
+        buttons: [
+            {
+                text: 'Next',
+                action: tour.next,
+
+                
+            }, {
+                text: 'Skip',
+                action: function() {
+                return tour.hide();
+              }
+            }
+        ]
+    });
+
+    tour.addStep('Tutorial', { 
+        text: 'Tap again to repeat the tour',
+        attachTo: '#infoButton right',
+        buttons: [
+             {
+                text: 'Done!',
+                action: function() {
+                return tour.complete();
+              }
+            }
+        ]
+    });
+
+
+function startTour() {
+    tour.start(); 
+
+}
