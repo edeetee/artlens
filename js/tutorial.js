b67a5bc81091045013f0a22b46e6d6105978a71f
ShakDesign <shakirajensen@hotmail.com>
2017-06-07 14:27:04 +1200

    tour now runs on first visit and on i button click

--- js/tutorial.js
+++ js/tutorial.js
@@ -7,30 +7,6 @@ var tour;
         }
     });
 
-    tour.addStep('Language', { 
-        text: 'Choose your language',
-        attachTo: '.goog-te-gadget-simple bottom',
-        tetherOptions:{ 
-            targetAttachment: 'bottom center',
-            offset: '0px 20px'
-        },
-        buttons: [
-            {
-                text: 'Next',
-                action: tour.next,
-
-                
-            }, {
-                text: 'Exit',
-                action: function() {
-                return tour.hide(),
-                $('#canvas').removeClass('dim'),
-                $('#video ').removeClass('blur');
-              }
-            }
-        ]
-    });
-
     tour.addStep('Camera', { 
         text: 'Tap the camera button to capture the object',
         attachTo: '#centerButton top',
@@ -71,8 +47,65 @@ var tour;
     });
 
 
-function startTour() {
-    tour.start(); 
-
+function startTour(){
+    tour.start();
+    
 }
 
+
+var firstTour;
+
+    firstTour = new Shepherd.Tour({
+        defaults: {
+            classes: 'shepherd-theme-dark',
+            /*scrollTo: true*/
+        }
+    });
+
+    firstTour.addStep('Camera', { 
+        text: 'Tap the camera button to capture the object',
+        attachTo: '#centerButton top',
+        buttons: [
+            {
+                text: 'Next',
+                action: firstTour.next,
+
+                
+            }, {
+                text: 'Exit',
+                action: function() {
+                return firstTour.hide(),
+                $('#canvas').removeClass('dim'),
+                $('#video ').removeClass('blur');
+              }
+            }
+        ]
+    });
+
+
+    firstTour.addStep('Tutorial', { 
+        text: 'Tap again to repeat the tour',
+        attachTo: '#infoButton right',
+        tetherOptions:{ 
+            targetAttachment: 'bottom right',
+        },
+        buttons: [
+             {
+                text: 'Done!',
+                action: function() {
+                return firstTour.complete(),
+                $('#canvas').removeClass('dim'),
+                $('#video ').removeClass('blur');
+              }
+            }
+        ]
+    });
+
+
+function startfirstTour(){
+        if (!localStorage.getItem('viewed')){
+           firstTour.start();  
+           localStorage.setItem('viewed','yes');
+    }
+    
+}
